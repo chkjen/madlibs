44e3f92afb6116a711ea1d4cdcd8a13e9109e4cc
chkjen <60936694+chkjen@users.noreply.github.com>
2022-07-21 11:59:33 -0500

    updated the lib to make more sense after initial creation

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ noun1 = input("Enter a noun (person, place or thing) : ")
 verb1 = input("Enter the first verb ( action, state or occurrence) :")
 verb2 = input("Enter the second verb ( action, state or occurrence) :")
 
-print(f"Hello, {noun1}. The first verb is {verb1}. The second verb is {verb2}.")
+print(f"The best thing about {noun1} is that they are really good at {verb1} as well as {verb2} when it suits.")
