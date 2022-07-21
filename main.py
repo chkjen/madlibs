fb8c80661bf8565b0ccd6598a778247bd5013202
chkjen <60936694+chkjen@users.noreply.github.com>
2022-07-21 11:24:59 -0500

    basic Madlibs app flesched out

---
+++ main.py
@@ -0,0 +1,6 @@
+
+noun1 = input("Enter a noun (person, place or thing) : ")
+verb1 = input("Enter the first verb ( action, state or occurrence) :")
+verb2 = input("Enter the second verb ( action, state or occurrence) :")
+
+print(f"Hello, {noun1}. The first verb is {verb1}. The second verb is {verb2}.")
